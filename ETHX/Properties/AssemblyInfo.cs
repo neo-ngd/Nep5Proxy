3ec9bc21a40d21aed12672c974042e223efe5eda
zhuoqian <zhuoqian10@gmail.com>
2020-04-17 13:58:14 +0800

    Update ETHX.

--- ETHX/Properties/AssemblyInfo.cs
+++ ETHX/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MockNep5")]
+[assembly: AssemblyTitle("ETHX")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MockNep5")]
+[assembly: AssemblyProduct("ETHX")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
